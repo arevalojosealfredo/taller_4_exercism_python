62afeb1169c9957635f945c317782c2d2f27c41b
Jose Arevalo <job.38011@gmail.com>
2020-06-17 01:17:27 -0500

    Solución realizada del primer ejercicio: <<Hamming>>

--- hamming/hamming.py
+++ hamming/hamming.py
@@ -1,2 +1,15 @@
 def distance(strand_a, strand_b):
-    pass
+    if len(strand_a) != len(strand_b):
+        raise ValueError("Las cadenas de ADN son de distintas longitudes")
+
+    elif len(strand_a) == len(strand_b):
+        num = 0
+        dis = 0
+        for dato in strand_a:
+            if dato != strand_b[num]:
+                dis += 1
+                num += 1
+            else:
+                num += 1
+        return dis
+    
