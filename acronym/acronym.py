ce0c79971f0c821924e3e77044800ecaa15e1e1e
Jose Arevalo <job.38011@gmail.com>
2020-06-17 10:23:18 -0500

    Solución realizada al quinto ejercicio: <<acronym>>

--- acronym/acronym.py
+++ acronym/acronym.py
@@ -1,2 +1,7 @@
 def abbreviate(words):
-    pass
+    tokens = words.replace("'", '').replace('-', ' ').replace('_', '').split()
+    # tokens = words.split()
+    result = ''
+    for item in tokens:
+        result += item[0].upper()
+    return result
