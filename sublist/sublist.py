41a7ac146f97246cc751fe062d68ac9d491229fa
Jose Arevalo <job.38011@gmail.com>
2020-06-17 13:15:27 -0500

    solución realizada al decimo ejercicio <<Sublist>>

--- sublist/sublist.py
+++ sublist/sublist.py
@@ -1,23 +1,19 @@
-"""
-This exercise stub and the test suite contain several enumerated constants.
-
-Since Python 2 does not have the enum module, the idiomatic way to write
-enumerated constants has traditionally been a NAME assigned to an arbitrary,
-but unique value. An integer is traditionally used because it’s memory
-efficient.
-It is a common practice to export both constants and functions that work with
-those constants (ex. the constants in the os, subprocess and re modules).
-
-You can learn more here: https://en.wikipedia.org/wiki/Enumerated_type
-"""
-
-# Possible sublist categories.
-# Change the values as you see fit.
-SUBLIST = None
-SUPERLIST = None
-EQUAL = None
-UNEQUAL = None
+SUBLIST = 'SUBLIST'
+SUPERLIST = 'SUPERLIST'
+EQUAL = 'EQUAL'
+UNEQUAL = 'UNEQUAL'
 
 
 def sublist(list_one, list_two):
-    pass
+    if list_one == list_two:
+        return EQUAL
+    elif len(list_one) < len(list_two) and list_one in slide(list_two, len(list_one)):
+        return SUBLIST
+    elif len(list_one) > len(list_two) and list_two in slide(list_one, len(list_two)):
+        return SUPERLIST
+    else:
+        return UNEQUAL
+
+
+def slide(lst, size):
+    return [lst[i:i+size] for i in range(len(lst)-size+1)]
