dbdc9ada2d1c9d652c3f1f527f2fc3e34b8fe6ec
Jose Arevalo <job.38011@gmail.com>
2020-06-17 10:51:54 -0500

    Solución realizada al sexto ejercicio: <<Darts>>

--- darts/darts.py
+++ darts/darts.py
@@ -1,2 +1,11 @@
+from math import sqrt
+
 def score(x, y):
-    pass
+    distancia = sqrt(x**2 + y**2)
+    if distancia <= 1:
+        return 10
+    elif distancia <= 5:
+        return 5
+    elif distancia <= 10:
+        return 1
+    return 0
