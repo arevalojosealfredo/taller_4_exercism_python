f6e9ba6e3bcbcc3e296d3ab2f5bbe1b5fe83547a
Jose Arevalo <job.38011@gmail.com>
2020-06-17 04:54:33 -0500

    solución realizada del cuarto ejercicio: <<Grains>>

--- grains/grains.py
+++ grains/grains.py
@@ -1,6 +1,9 @@
 def square(number):
-    pass
+    if number <= 0 or number > 64:
+        raise ValueError("error")
+
+    return int(2 ** (number - 1))
 
 
 def total():
-    pass
+    return sum(square(i) for i in range(1, 65))
