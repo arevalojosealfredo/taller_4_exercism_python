0b79e5e961b2ea0296a9a1b8cc5cc0ab87d9c2c9
Jose Arevalo <job.38011@gmail.com>
2020-06-17 12:33:40 -0500

    Solución realizada al noveno ejercicio <<Transpose>>

--- transpose/transpose.py
+++ transpose/transpose.py
@@ -1,2 +1,8 @@
+from itertools import zip_longest
+
+
 def transpose(lines):
-    pass
+    return "\n".join(
+        "".join(block).rstrip("\n").replace("\n", " ") for block
+        in zip_longest(*lines.splitlines(), fillvalue="\n")
+    )
